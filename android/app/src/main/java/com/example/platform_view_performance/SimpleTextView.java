be267b439d201e75b0be197121c3bcc46b68c94b
Rulong Chen（陈汝龙） <rulong.crl@alibaba-inc.com>
2023-03-16 15:40:33 +0800

    Add `OnTouchListener` for PlatformViews.

--- android/app/src/main/java/com/example/platform_view_performance/SimpleTextView.java
+++ android/app/src/main/java/com/example/platform_view_performance/SimpleTextView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
@@ -13,47 +14,62 @@ import java.util.Map;
 import java.util.Random;
 
 class SimpleTextView implements PlatformView {
-   @NonNull private final TextView textView;
-   private int id;
+  private final String TAG = "SimpleTextView";
+  @NonNull private final TextView textView;
+  private int id;
 
-   SimpleTextView(@NonNull Context context, int id, @Nullable Map<String, Object> creationParams) {
-        this.id = id;
-        textView = new TextView(context);
-        textView.setTextSize(16);
+  private int getRandomColor() {
+    Random rnd = new Random();
+    return Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+  }
 
-        Random rnd = new Random();
-        int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
-        textView.setBackgroundColor(color);
-        textView.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+  SimpleTextView(@NonNull Context context, int id, @Nullable Map<String, Object> creationParams) {
+    this.id = id;
+    textView = new TextView(context);
+    textView.setTextSize(16);
 
-        StringBuilder sb = new StringBuilder();
-        sb.append("Native Android view (id: " + id + ")\n\n");
-        for (Map.Entry<String, Object> entry : creationParams.entrySet()) {
-            sb.append(entry.getKey() + ": " + entry.getValue().toString()).append("\n");
+    textView.setBackgroundColor(getRandomColor());
+    textView.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+
+    StringBuilder sb = new StringBuilder();
+    sb.append("Native Android view (id: " + id + ")\n\n");
+    for (Map.Entry<String, Object> entry : creationParams.entrySet()) {
+      sb.append(entry.getKey() + ": " + entry.getValue().toString()).append("\n");
+    }
+    textView.setTextColor(Color.WHITE);
+    textView.setText(sb.toString());
+
+    textView.setOnTouchListener(new View.OnTouchListener() {
+      @Override
+      public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+          textView.setBackgroundColor(getRandomColor());
         }
-        textView.setTextColor(Color.WHITE);
-        textView.setText(sb.toString());
-        Log.e("xlog", "#SimpleTextView: <ctor> " + sb.toString());
-    }
+        Log.e(TAG, "#onTouch " + event.getAction());
+        return true;
+      }
+    });
+    Log.e(TAG, "#SimpleTextView: <ctor> " + sb.toString());
+  }
 
-    @NonNull
-    @Override
-    public View getView() {
-        return textView;
-    }
+  @NonNull
+  @Override
+  public View getView() {
+    return textView;
+  }
 
-    @Override
-    public void onFlutterViewAttached(@NonNull View flutterView) {
-        Log.e("xlog", "#SimpleTextView#onFlutterViewAttached, " + flutterView);
-    }
+  @Override
+  public void onFlutterViewAttached(@NonNull View flutterView) {
+    Log.e(TAG, "#SimpleTextView#onFlutterViewAttached, " + flutterView);
+  }
 
-    @Override
-    public void onFlutterViewDetached() {
-        Log.e("xlog", "#SimpleTextView#onFlutterViewDetached");
-    }
+  @Override
+  public void onFlutterViewDetached() {
+    Log.e(TAG, "#SimpleTextView#onFlutterViewDetached");
+  }
 
-    @Override
-    public void dispose() {
-        Log.e("xlog", "#SimpleTextView#dispose~~ id=" + id);
-    }
+  @Override
+  public void dispose() {
+    Log.e(TAG, "#SimpleTextView#dispose~~ id=" + id);
+  }
 }
